c8b307c23112f8dcf87797103d528964daf0a124
Michael Tang <22377395+MTang0728@users.noreply.github.com>
2020-12-10 13:13:34 -0500

    updated application.py to include forecast, added radioitems to include forecasted trends

--- application.py
+++ application.py
@@ -4,13 +4,18 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import pandas as pd
+import numpy as np
 
 from fbprophet import Prophet
 
 import plotly.express as px
 import plotly.graph_objects as go
 
+#---------------------------------------------------------------------------------------------
+
 app = dash.Dash(__name__)
+
+#---------------------------------------------------------------------------------------------
 
 def clean_data(trends):
     """Clean data to desired format
@@ -87,17 +92,63 @@ def get_country_forecast(forecast_countries, country_names, country_name = 'Unit
         
         return forecast_country
 
+def add_trend(country, trend, forecast, include_forecast):
+    """creates a line plot and adds historical and forecasted trend based on country
+    Input:
+        country (string): country name
+        trend (dataframe): historical trends for all countries
+                           hierarchical columns by 'country' and 'transportation type'
+                           indexed are dates
+        forecast (dataframe): forecasted trends for all countries
+                              hierarchical columns by 'country' and 'transportation type'
+                              indexed are dates
+        include_forecast (boolean): whether or not to include forecasted trends
+    Output 
+        fig (plotly express figure): line plot
+    """
+    # define line colors for 3 transportation types
+    line_color = np.array(['#636EFA', '#EF553B', '#00CC96'])
+    # get historical and forecasted trends for a country
+    country_trend = get_country_trend(trend, country_names, country)
+    country_forecast = get_country_forecast(forecast, country_names, country)
+    # create a line plot
+    fig = px.line(template = 'plotly_dark')
+    # loop through transportation types and add corresponding trend lines
+    for idx, transportation in enumerate(country_trend.columns):
+        # add historical trend line
+        fig.add_scatter(x = country_trend.index, 
+                        y = country_trend[transportation],
+                        line = dict(color = line_color[idx]),
+                        name = transportation)
+        # add forecasted trend line if include_forecast is True
+        if include_forecast:
+            fig.add_scatter(x = country_forecast.index,
+                            y = country_forecast[transportation],
+                            line = dict(color = line_color[idx],
+                                        dash='dash'),
+                            showlegend = False)
+            pass
+        else:
+            pass
+    
+    return fig
+
+#---------------------------------------------------------------------------------------------
+
+# read in the historical trend data and forecasted trend data
 trend_data = pd.read_csv('./data/applemobilitytrends-2020-12-08.csv', 
                          parse_dates = True, 
                          low_memory = False)
-
 trends_countries, country_names = clean_data(trend_data)
-forecast_countries = pd.read_csv('./data/forecasted_trends.csv')
+forecast_countries = pd.read_csv('./data/forecasted_trends.csv', header = [0,1], index_col = 0)
 
+#---------------------------------------------------------------------------------------------
 
 # define most recent trend by taking the mean of transportation types
 most_recent_trends = [trends_countries[c].iloc[-1, :].mean().round(2) for c in country_names]
+# use reversed color scale for map
 color_scale = list(reversed(px.colors.sequential.Oryel))
+# create a choropleth geo map
 fig_map = px.choropleth(locations = country_names,
                     locationmode = "country names",
                     color = most_recent_trends,
@@ -106,35 +157,49 @@ fig_map = px.choropleth(locations = country_names,
                     projection = "natural earth",
                     template = 'plotly_dark',
                     height = 500)
+# update choropleth map specs
 geo = dict(projection_type = "natural earth",
            countrycolor = "RebeccaPurple", landcolor = 'silver',
            showocean = True, oceancolor = "rgb(136,204,238)", lakecolor = "rgb(136,204,238)",
-           showland = True)
+           showland = True
+          )
 fig_map.update_geos(geo)
 
+#---------------------------------------------------------------------------------------------
+
+available_trends = ['No', 'Yes']
+
+# define dashboard layout
 app.layout = html.Div([
     dcc.Graph(id = 'world_map',
               figure = fig_map,
               hoverData = {'points': [{'hovertext': 'United States'}]}
              ),
+    html.Div([
+        html.Div('Include a 30-Day Forecast: '),
+        dcc.RadioItems(id = 'include_forecast',
+                       options = [{'label': i, 'value': i} for i in available_trends],
+                       value = 'No',
+                       labelStyle = {'display': 'inline-block'}
+                      ),
+             ]),
     dcc.Graph(id = 'trend')
 ])
 
-def add_trend(country, trend):
-    line_color = ['#636EFA', '#EF553B', '#00CC96']
-    country_trend = get_country_trend(trend, country_names, country)
-    fig = px.line(country_trend,
-                  template = 'plotly_dark')
-    
-    return fig
+#---------------------------------------------------------------------------------------------
 
 @app.callback(
-    Output(component_id='trend', component_property='figure'),
-    Input(component_id='world_map', component_property='hoverData')
+    Output(component_id = 'trend', component_property = 'figure'),
+    [Input(component_id = 'world_map', component_property = 'hoverData'),
+     Input(component_id = 'include_forecast', component_property = 'value')]
 )
-def update_output_div(input_value):
-    country = input_value['points'][0]['hovertext']
-    return add_trend(country, trends_countries)
+def update_output_div(input_map_value, input_radioitem_value):
+    # get country name from hoverData
+    country = input_map_value['points'][0]['hovertext']
+    # convert include forecast selection to boolean
+    include_forecast = True if input_radioitem_value == 'Yes' else False
+    
+    return add_trend(country, trends_countries, forecast_countries, include_forecast)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug = True)
